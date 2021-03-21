d36152d57a71a3dbb1fded988bc0e238aca84a26
Bernhard Winkler <Epyon@live.at>
2021-03-21 19:25:03 +0100

    bugfix

--- physx/source/geomutils/src/gjk/GuGJKType.h
+++ physx/source/geomutils/src/gjk/GuGJKType.h
@@ -149,7 +149,7 @@ namespace Gu
 			return getConvex<Convex>().supportRelative(dir, mAToB, mAToBTransposed, index);
 		}
 
-		virtual Ps::aos::Vec3V getCenter() const { return mAToB.transform(getConvex<Convex>().getCenter()); }
+		virtual const Ps::aos::Vec3V getCenter() const { return mAToB.transform(getConvex<Convex>().getCenter()); }
 
 		PX_FORCE_INLINE Ps::aos::PsMatTransformV& getRelativeTransform(){ return mAToB; }
 
